886d7ed3714fd8ca04d0e5654aa6ca4f0bfb4ab0
jtreeves <jackson.t.reeves@gmail.com>
2024-05-19 21:18:09 -0400

    Ensure all types of tests have examples and can be run successfully

--- test/integration_tests/app_test.dart
+++ test/integration_tests/app_test.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 import 'package:wordiest/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('tap on the floating action button, verify counter',
+      (tester) async {
     await tester.pumpWidget(const MyApp());
 
     expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
 
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    final fab = find.byKey(const Key('increment'));
+
+    await tester.tap(fab);
+    await tester.pumpAndSettle();
 
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
